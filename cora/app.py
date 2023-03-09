95da109f257dd217729ed2361775b20864961051
Kira Schmitt <schmitt@zib.de>
2023-03-09 18:13:17 +0100

    cleaned up code added hover (tooltip) tool

--- cora/app.py
+++ cora/app.py
@@ -60,18 +60,14 @@ logging.info("Creating SPLOM ...")
 # Create some fake labels for testing.
 ipc_features.df["label"] = np.random.randint(0, 5, ipc_features.df.shape[0]).astype(str)
 
-print(ipc_features.df["label"])
-
-
+# Choose the currently active columns.
 features_df = ipc_features.df
 features_columns = ["Volume3d", "Area3d", "BaryCenterZ", "label"]
 features_bokeh = bokeh.models.ColumnDataSource(features_df)
 
 
-def splom_selection_callback():
+def features_splom_selection_callback():
     """Called when the user selection changes."""
-
-
     return None
 
 
@@ -79,62 +75,54 @@ def features_splom():
     """Shows a scatterplot matrix (SPLOM) for the selected
     raw features of the :data:`ipc_features` spreadsheet.
     """
-    global features_bokeh, features_df, features_columns
-
+    global features_bokeh
+    global features_df 
+    global features_columns
+    
+    # Create shortcuts for the data source.
     df = features_df
     source = features_bokeh
-
     columns = features_columns
     ncolumns = len(columns) - 1
-
-    print(df)
 
     # Create the ranges.
     x_ranges = []
     y_ranges = []
     for i in range(ncolumns):
         column = columns[i]
-        df_column = df[column]
-        vmin = df_column.min()
-        vmax = df_column.max()
-        x_ranges.append(bokeh.models.Range1d(vmin, vmax, bounds=(vmin, vmax), name=f"x_range_{column}"))
-        y_ranges.append(bokeh.models.Range1d(vmin, vmax, bounds=(vmin, vmax), name=f"y_range_{column}"))
+        values = df[column]
+        vmin = values.min()
+        vmax = values.max()        
+        x_range = bokeh.models.Range1d(vmin, vmax, bounds=(vmin, vmax), name=f"x_range_{column}")
+        y_range = bokeh.models.Range1d(vmin, vmax, bounds=(vmin, vmax), name=f"y_range_{column}")
+        x_ranges.append(x_range)
+        y_ranges.append(y_range)
 
     # Create the axes.
     x_axes = []
     y_axes = []
     for i in range(ncolumns):
         column = columns[i]
-        df_column = df[column]
-        x_axis = bokeh.models.LinearAxis(
-            axis_label=df.columns[i],
-            x_range_name=f"x_range_{column}"
-        )
-        y_axis = bokeh.models.LinearAxis(
-            axis_label=df.columns[i],
-            y_range_name=f"y_range_{column}"
-        )
+        values = df[column]
+        x_axis = bokeh.models.LinearAxis(axis_label=df.columns[i], x_range_name=f"x_range_{column}")
+        y_axis = bokeh.models.LinearAxis(axis_label=df.columns[i], y_range_name=f"y_range_{column}")
         x_axes.append(x_axis)
         y_axes.append(y_axis)
 
-    # Initialise the SPLOM grid.
-    grid = []
+    # Prepare the label / classes for the histogram plots.
+    labels = df["label"]
+    labels_unique = np.sort(np.unique(labels))
+    labels2id = {label: i for i, label in enumerate(labels_unique)}
+    labels_int = np.array([labels2id[label] for label in df["label"]])
 
-    tools = {
-        "pan": bokeh.models.PanTool(),
-        "lasso": bokeh.models.LassoSelectTool(),
-        "box_zoom": bokeh.models.BoxZoomTool(),
-        "wheel_zoom": bokeh.models.WheelZoomTool(),
-        "reset": bokeh.models.ResetTool()
-    }
-
+    # Use the labels (classes) as colormap.
     colormap = bokeh.transform.factor_cmap(
-        "label", palette=bokeh.palettes.Spectral5, factors=sorted(df["label"].unique())
+        "label", palette=bokeh.palettes.Spectral5, factors=labels_unique.astype(str)
     )
-    
 
+    # Create the SPLOT plots.
+    grid = []
     for irow in range(ncolumns):
-
         # Add a new row to the grid.
         row = []
         grid.append(row)
@@ -143,91 +131,78 @@ def features_splom():
             x_range = x_ranges[icol]
             y_range = y_ranges[irow]
 
+            # Create a histogram for the plots on the SPLOM diagonal.
             if irow == icol:
+                x_values = df[columns[icol]]
+                x_min = x_values.min()
+                x_max = x_values.max()
+                x_range = (x_min, x_max)
+                x_bins = 10
                 
-                xvalues = df[columns[icol]]
-                xmin = xvalues.min()
-                xmax = xvalues.max()
-                xrange = (xmin, xmax)
-                xbins = 10
-                
-                labels = df["label"].astype(int).unique()
-                label2id = {labels[i]: i for i in range(len(labels))}
-                yvalues = np.arange(len(labels))
-                ymin = 0
-                ymax = len(labels)
-                yrange = (ymin, ymax)
-                ybins = len(labels)
+                y_min = 0
+                y_max = len(labels_unique)
+                y_range = (y_min, y_max)
+                y_bins = len(labels_unique)
                 
                 hist, xedges, yedges = np.histogram2d(
-                    x=xvalues, y=df["label"].astype(int),
-                    bins=(xbins, ybins), range=(xrange, yrange)
+                    x=x_values, 
+                    y=labels_int,
+                    bins=(x_bins, y_bins), 
+                    range=(x_range, y_range)
                 )
 
-                # hist = np.sum(hist, axis=1)
-                
-                print()
-                print(yedges)
-                print(xedges)
-                print(hist)
-
-                data = {
-                    str(i): hist[:, i] for i in range(len(labels))
-                }
-                # data["label"] = labels.astype(str)
+                # Pack the histogram data in a dictionary for bokeh to process.
+                data = {labels_unique[i]: hist[:, i] for i in range(len(labels_unique))}
                 data["xedges"] = (xedges[:-1] + xedges[1:])/2.0
-                # data["xedges"] = np.arange(10).astype(str)
-                from pprint import pprint
-                pprint(data)
 
+                # Compute the bin width for the stack area widgets.
+                bin_width = np.min(np.abs(np.diff(data["xedges"])))
+                max_bin_count = np.max(np.sum(hist, axis=1))
+
+                # TODO: Link the colormap client-side.
                 colors = bokeh.palettes.Spectral5
 
-                # TODO: 
-
-                # colors = ["#c9d9d3", "#718dbf", "#e84d60", "#c9d9d3", "#718dbf"]
-
-                # TODO: Compute a histogram for each label and draw a stacked bar chart.
-                # values = df[df.columns[icol]]
-                # hist, bin_edges = np.histogram(values, bins=10)
-                # y_range = bokeh.models.Range1d(0, hist.max(), bounds=(0, hist.max()))
-                # p = bokeh.plotting.figure(width=250, height=250, x_range=x_range, y_range=y_range)
-                # p.quad(top=hist, bottom=0, left=bin_edges[:-1], right=bin_edges[1:], line_color="white")
-
-                max_count = np.max(np.sum(hist, axis=1))
-                print(max_count)
-                y_range = bokeh.models.Range1d(0, max_count, bounds=(0, max_count))
+                # Create the histogram plot.
                 p = bokeh.plotting.figure(
-                    width=250, height=250, #x_range=np.arange(10).astype(str)
-                    x_range=x_range, y_range=y_range)
-
-                p.vbar_stack(labels.astype(str), x="xedges", source=data, fill_color=colors, line_color="white", width=np.max(np.abs(np.diff(data["xedges"]))))
-
+                    width=250, height=250, x_range=x_range, 
+                    y_range=bokeh.models.Range1d(0, max_bin_count, bounds=(0, max_bin_count))
+                )
+                p.vbar_stack(
+                    labels_unique, x="xedges", source=data, fill_color=colors, 
+                    line_color="white", width=bin_width
+                )
                 p.xaxis.visible = False
                 p.yaxis.visible = False
                 p.xgrid.visible = False
                 p.ygrid.visible = False
 
+            # Create scatter plots for upper-diagonal plots.
             elif icol > irow:
                 p = bokeh.plotting.figure(
                     width=250, height=250, x_range=x_range, y_range=y_range,
-                    tools=list(tools.values())
+                    tools="pan,lasso_select,box_zoom,wheel_zoom,reset,hover"
+                )
+                p.scatter(
+                    source=source, x=columns[icol], y=columns[irow], 
+                    color=colormap, alpha=0.6, size=8.0
+                    
                 )
                 p.xaxis.visible = False
                 p.yaxis.visible = False
-
-                p.scatter(
-                    source=source, x=columns[icol], y=columns[irow], alpha=0.8, size=4.0,
-                    color=colormap
-                )
 
             else:
                 p = None
 
             row.append(p)
 
-    # Create "fake" plots for the y-axes.
+    # Create "fake" plots for the y-axes. The only purpose for these
+    # plots is to show the axis on top of the SPLOM.
     for irow in range(ncolumns):
-        p = bokeh.plotting.figure(width=80, height=250, x_range=x_ranges[irow], y_range=y_ranges[irow], y_axis_location="right", outline_line_color=None)
+        p = bokeh.plotting.figure(
+            width=80, height=250, 
+            x_range=x_ranges[irow], y_range=y_ranges[irow], 
+            y_axis_location="right", outline_line_color=None
+        )
         p.scatter([], [])
         p.xaxis.visible = False
         p.xgrid.visible = False
@@ -237,9 +212,15 @@ def features_splom():
         p.yaxis.ticker.desired_num_ticks = 4
         grid[irow].append(p)
 
+    # Create "fake" plots for the x-axes. The only purpose for these
+    # plots is to show the axis on the right side of the SPLOM.
     grid.insert(0, [])
     for icol in range(ncolumns):
-        p = bokeh.plotting.figure(width=250, height=60, x_range=x_ranges[icol], y_range=y_ranges[icol], x_axis_location="above", outline_line_color=None)
+        p = bokeh.plotting.figure(
+            width=250, height=60, 
+            x_range=x_ranges[icol], y_range=y_ranges[icol], 
+            x_axis_location="above", outline_line_color=None
+        )
         p.scatter([], [])
         p.yaxis.visible = False
         p.xgrid.visible = False
@@ -248,37 +229,37 @@ def features_splom():
         p.xaxis.axis_label = df.columns[icol]
         p.xaxis.ticker.desired_num_ticks = 4
         grid[0].append(p)
+
+    # Create an empty plot in the top-right corner.
     grid[0].append(None)
 
+    # Wrap everything in a proper bokeh layout.
     grid = bokeh.layouts.gridplot(grid)
-
-    # grid.toolbar.active_drag = tools["pan"]
-    # grid.toolbar.active_inspect = tools["wheel"]
-    # grid.toolbar.active_scroll = tools["wheel"]
     grid.toolbar_location = "right"
-    
-    
     return grid
 
 
 def features_table():
-    """Shows the :data:`ipc_spreadsheet` raw features as a spreadsheet."""
-    global features_df, features_bokeh
+    """Shows the :data:`ipc_spreadsheet` raw features as a spreadsheet.
+    Only the columns which are currently selected by the user are displayed.
+    """
+    global features_df
+    global features_bokeh
+    global features_columns
 
+    # Use some shortcuts.
     df = features_df
     source = features_bokeh
     columns = features_columns
-
-
-    columns = [
-        bokeh.models.TableColumn(field=name, title=name) for name in columns
-    ]
-    data_table = bokeh.models.DataTable(
+    
+    # Create a column for each feature.
+    table_columns = [bokeh.models.TableColumn(field=name, title=name) for name in columns]
+    table = bokeh.models.DataTable(
         source=source, columns=columns, sizing_mode="stretch_both",
         selectable=True, sortable=True, syncable=True, autosize_mode="fit_columns",
-        scroll_to_selection=True
+        scroll_to_selection=True, reorderable=True
     )
-    return data_table
+    return table
 
 
 def umap_thread():
@@ -403,7 +384,7 @@ tabs = bokeh.models.Tabs(tabs=[
     bokeh.models.TabPanel(child=graph(), title="Graph"),
     bokeh.models.TabPanel(child=graph_vertex_table(), title="Graph Vertex Table"),
     bokeh.models.TabPanel(child=graph_edge_table(), title="Graph Edge Table")
-], active=1, sizing_mode="stretch_both")
+], active=0, sizing_mode="stretch_both")
 
 document = bokeh.plotting.curdoc()
 document.add_root(tabs)
