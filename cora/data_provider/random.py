480735632fa2118caaca3212cba26156cf266840
Kira Schmitt <schmitt@zib.de>
2023-10-10 16:30:23 +0200

    replaced pprint with print

--- cora/data_provider/random.py
+++ cora/data_provider/random.py
@@ -72,25 +72,25 @@ class RandomDataProvider(DataProvider):
 
     def write_vertex_selection(self, indices):
         print("-- begin: vertex selection --")
-        pprint(indices)
+        print(indices)
         print("-- end: vertex selection --")
         return None
 
     def write_edge_selection(self, indices):
         print("-- begin: edge selection --")
-        pprint(indices)
+        print(indices)
         print("-- end: edge selection --")
         return None
 
     def write_vertex_colormap(self, colors):
         print("-- begin: vertex colormap --")
-        pprint(colors)
+        print(colors)
         print("-- end: vertex colormap --")
         return None
 
     def write_edge_colormap(self, colors):
         print("-- begin: edge colormap --")
-        pprint(colors)
+        print(colors)
         print("-- end: edge colormap --")
         return None
         
