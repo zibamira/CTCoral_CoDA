2850d06fa46b65507bccdab56e230ad3a74893c2
Kira Schmitt <schmitt@zib.de>
2024-04-23 15:38:48 +0200

    commented out framework

--- inter_colony.py
+++ inter_colony.py
@@ -19,11 +19,12 @@ bremen_dir = (this_dir / "../BremenExploreScience").absolute()
 
 # Add some meta data to the colonies themselves.
 colony_infos = dict()
-# colony_infos["A2W"] = {
-#     "latitude": 0.0,
-#     "longitude": 0.0,
-#     "elevation": 0.0
-# }
+colony_infos["A2W"] = {
+    "latitude": 63.3646,
+    "longitude": 9.2276,
+    "elevation": -157.0,
+    "data_dir": bremen_dir / "A2W"
+}
 
 # colony_infos["B2R"] = {
 #     "latitude": 0.0,
@@ -70,18 +71,18 @@ dfs_info = []
 for name, info in colony_infos.items():
     df_corals = pd.read_csv(info["data_dir"] / "corals.csv", header=1)
     df_corallites = pd.read_csv(info["data_dir"] / "corallites.csv", header=1)
-    df_framework = pd.read_csv(info["data_dir"] / "framework.csv", header=1)
+    # df_framework = pd.read_csv(info["data_dir"] / "framework.csv", header=1)
     
-    if "Node.label" in df_framework:
-        df_framework["Node.Label"] = df_framework["Node.label"]
+    # if "Node.label" in df_framework:
+    #     df_framework["Node.Label"] = df_framework["Node.label"]
 
     nrows = len(df_corals.index)
     if len(df_corals.index) != nrows:
         print(f"{name} corals has incosistent number of rows {len(df_corals.index)}, expected {nrows}.")
     if len(df_corallites.index) != nrows:
         print(f"{name} corallites has incosistent number of rows {len(df_corallites.index)}, expected {nrows}.")
-    if len(df_framework.index) != nrows:
-        print(f"{name} framework has incosistent number of rows {len(df_framework.index)}, expected {nrows}.")
+    # if len(df_framework.index) != nrows:
+    #     print(f"{name} framework has incosistent number of rows {len(df_framework.index)}, expected {nrows}.")
 
     df_info = pd.DataFrame.from_dict({
         "latitude": [info["latitude"] for i in range(nrows)],
@@ -96,14 +97,14 @@ for name, info in colony_infos.items():
     prefixes.append(prefix)
     dfs_corals.append(df_corals)
     dfs_corallites.append(df_corallites)
-    dfs_framework.append(df_framework)
+    # dfs_framework.append(df_framework)
     dfs_info.append(df_info)
 
 
 # Check if we need to get rid of some columns.
 columns_corals = set.intersection(*[set(df.columns) for df in dfs_corals])
 columns_corallites = set.intersection(*[set(df.columns) for df in dfs_corallites])
-columns_framework = set.intersection(*[set(df.columns) for df in dfs_framework])
+# columns_framework = set.intersection(*[set(df.columns) for df in dfs_framework])
 columns_info = set.intersection(*[set(df.columns) for df in dfs_info])
 
 # Give a warning when we remove a column.
@@ -130,13 +131,13 @@ for prefix, df in zip(prefixes, dfs_info):
 # Sort the columns by name.
 columns_corals = list(sorted(columns_corals))
 columns_corallites = list(sorted(columns_corallites))
-columns_framework = list(sorted(columns_framework))
+# columns_framework = list(sorted(columns_framework))
 columns_info = list(sorted(columns_info))
 
 # Concatenate all dataframes.
 df_corals = pd.concat(df[columns_corals] for prefix, df in zip(prefixes, dfs_corals))
 df_corallites = pd.concat(df[columns_corallites] for prefix, df in zip(prefixes, dfs_corallites))
-df_framework = pd.concat(df[columns_framework] for prefix, df in zip(prefixes, dfs_framework))
+# df_framework = pd.concat(df[columns_framework] for prefix, df in zip(prefixes, dfs_framework))
 df_info = pd.concat(df[columns_info] for prefix, df in zip(prefixes, dfs_info))
 
 # Save the compiled spreadsheets.
@@ -148,9 +149,9 @@ with (this_dir / "data" / "vertex_corallites.csv").open("w") as file:
     file.write("\"CORA compilation\"\n")
     df_corallites.to_csv(file, sep=",", header=True, index=False)
 
-with (this_dir / "data" / "vertex_framework.csv").open("w") as file:
-    file.write("\"CORA compilation\"\n")
-    df_framework.to_csv(file, sep=",", header=True, index=False)
+# with (this_dir / "data" / "vertex_framework.csv").open("w") as file:
+#     file.write("\"CORA compilation\"\n")
+#     df_framework.to_csv(file, sep=",", header=True, index=False)
 
 with (this_dir / "data" / "vertex_info.csv").open("w") as file:
     file.write("\"CORA compilation\"\n")
